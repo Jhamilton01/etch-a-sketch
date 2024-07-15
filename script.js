b8fc6a27764c8892b7eb602d91f5843da780dd27
Jastine Hamilton <hamiltonchase14@gmail.com>
2024-07-15 00:24:02 -0400

    Version 2.0 - Update Completed the functionality of being able to change the grid size each square in the grid now changes to a random color added a clear button to erase the current grid but keep dimension

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const divContainer = document.querySelector("#container")
+const changeBtn = document.querySelector("#newGrid")
+const clearBtn = document.querySelector("#clear")
+let gridColumn = 16
+let gridRow = 16
 
 function main() {
-    createGrid()
+    // Starting size 16x16
+    createGrid(gridRow, gridColumn)
+    changeGrid()
+    clear()
 }
 
 
-//creates a 16x16 grid made out of divs
-function createGrid(){
-    for (let i = 0; i < 256; i++){
-        const square = document.createElement('div')
+function createGrid(gridRow, gridColumn){
+    const containerWidth = 400;
+
+    const squareWidth = containerWidth / gridColumn
+    divContainer.style.width = `${containerWidth}px`
+
+    for (let i = 0; i < (gridRow * gridColumn); i++){
+        let square = document.createElement('div')
+        square.style.width = `${squareWidth}px`
+        square.style.height = `${squareWidth}px`
+        /* flex basis was used to make it so only 16 items per row of 100% of the div container*/
+        square.style.flexBasis = `calc(100% / ${gridColumn})`
         square.className = `square-${i+1}`
         divContainer.appendChild(square)
         hover(square)
@@ -19,12 +34,49 @@ function createGrid(){
 
 function hover(square){
     square.addEventListener('mouseenter', () => {
-        square.style.backgroundColor = 'yellow'
+        square.style.backgroundColor = getRandomColor()
     })
 
     square.addEventListener('mouseleave', () => {
-        square.style.backgroundColor = 'yellow'
+        square.style.backgroundColor = getRandomColor()
     })
+}
+
+function getRandomColor(){
+    const letters = '0123456789ABCDEF'
+    let color = '#'
+    for (let i =0;i<6;i++){
+        color+=letters[Math.floor(Math.random()*16)]
+    }
+    return color
+}
+function changeGrid(){
+    changeBtn.addEventListener('click', () => {
+        while (true) {
+            gridRow = prompt("Enter the number of rows:");
+            gridColumn = prompt("Enter the number of columns:");
+    
+            if (gridRow < 100 && gridColumn < 100) {
+                break;
+            } else {
+                alert("Both column and row should be less than 100. Please try again.");
+            }
+        }
+    
+            divContainer.textContent = ''
+            createGrid(gridRow, gridColumn)
+        })
+}
+
+function clear(){
+    //the .children is used to get the children of t given id
+    const squares = divContainer.children
+    clearBtn.addEventListener('click', () => {
+        for (let square of squares){
+            square.style.backgroundColor = 'lightgray'
+        }
+    })
+
 }
 
 main()
